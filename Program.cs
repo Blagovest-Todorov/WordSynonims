4a3eec2264c88101cbf3cf486d8be052ad432523
Blagovest-Todorov <blagovest.stod@abv.bg>
2021-03-02 13:31:34 +0200

    add Program.cs

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfwords = int.Parse(Console.ReadLine());
+            Dictionary<string, List<string>> dictionary = new Dictionary<string, List<string>>();
+
+            for (int i = 0; i < numberOfwords; i++)
+            {
+                string word = Console.ReadLine();
+                string synonim = Console.ReadLine();
+
+                if (dictionary.ContainsKey(word))
+                {
+                    dictionary[word].Add(synonim);
+                }
+                else // (!dictionary.ContainsKey("word"))
+                {
+                    dictionary.Add(word, new List<string>() { synonim});
+                }
+            }
+
+            foreach (var synonim in dictionary)
+            {
+                Console.WriteLine($"{synonim.Key} - {string.Join(", " , synonim.Value)}");
+            }
+        }
+    }
+}
